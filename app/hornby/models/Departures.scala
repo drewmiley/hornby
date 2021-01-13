212d36ef8c28d1982de39784cc04f9f62bca5495
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2021-01-13 23:57:22 +0000

    Initial dummy models for Arrivals, Departures, Detailed Service

---
+++ app/hornby/models/Departures.scala
@@ -0,0 +1,36 @@
+package hornby.models
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+case class Departures(
+                     dummy: String = "",
+                     dummy1: String = ""
+                   )
+
+object Departures {
+  implicit val reads: Reads[Departures] = (
+    (__ \ "dummy").read[String] and
+      (__ \ "dummy1").read[String]
+    )(Departures.apply _)
+}
+
+//case class Leaderboard(code: String = "", rows: Seq[LeaderboardRow] = Seq()) {
+//  val rowsWithPosition: Seq[LeaderboardRow] = rows
+//    .sorted(Ordering.by[LeaderboardRow, Int](_.score).reverse)
+//    .foldLeft(Seq[LeaderboardRow]())((acc, row) => {
+//      val position: Int = if (acc.nonEmpty && row.score == acc.last.score) {
+//        acc.last.positionAsString.toInt
+//      } else {
+//        acc.length + 1
+//      }
+//      acc :+ LeaderboardRow(row.score, row.user, Some(position))
+//    })
+//}
+//
+//object Leaderboard {
+//  implicit val reads: Reads[Leaderboard] = (
+//    (__ \ "code").read[String] and
+//      (__ \ "results").read[Seq[LeaderboardRow]]
+//    )(Leaderboard.apply _)
+//}
