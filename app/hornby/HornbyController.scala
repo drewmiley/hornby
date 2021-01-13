9357ae9abb2d0706b385384da6686449fe6e3bc4
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2021-01-13 19:51:30 +0000

    Reverts new folder addition

--- app/hornby/HornbyController.scala
+++ app/hornby/HornbyController.scala
@@ -1,25 +1,25 @@
-//package hornby
-//
-//import play.api.libs.json.Json
-//import play.api.mvc._
-//import play.api.{Configuration, Logger}
-//
-//import javax.inject.Inject
-//import scala.concurrent.ExecutionContext
-//
-///**
-//  * Takes HTTP requests and produces JSON.
-//  */
-//class HornbyController @Inject()(hornbyService: HornbyService, cc: HornbyControllerComponents, configuration: Configuration)(
-//    implicit ec: ExecutionContext)
-//    extends HornbyBaseController(cc) {
-//
-//  private val logger = Logger(getClass)
-//
-//  def test: Action[AnyContent] = HornbyAction.async { implicit request =>
-//    logger.trace("test: ")
-//    hornbyService.getStationData("NEW").map { data =>
-//      Ok(Json.toJson(data))
-//    }
-//  }
-//}
+package hornby
+
+import play.api.libs.json.Json
+import play.api.mvc._
+import play.api.{Configuration, Logger}
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+/**
+  * Takes HTTP requests and produces JSON.
+  */
+class HornbyController @Inject()(hornbyService: HornbyService, cc: HornbyControllerComponents, configuration: Configuration)(
+    implicit ec: ExecutionContext)
+    extends HornbyBaseController(cc) {
+
+  private val logger = Logger(getClass)
+
+  def test: Action[AnyContent] = HornbyAction.async { implicit request =>
+    logger.trace("test: ")
+    hornbyService.getStationData("NEW").map { data =>
+      Ok(Json.toJson(data))
+    }
+  }
+}
