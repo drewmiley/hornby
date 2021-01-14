f36fc029b31d95ef6692f5f90e86b58093a60c62
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2021-01-14 02:05:46 +0000

    CallingPoints now compile

--- app/hornby/models/DetailedService.scala
+++ app/hornby/models/DetailedService.scala
@@ -4,20 +4,23 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 case class DetailedService(
-//                       previousCallingPoints: Seq[CallingPoint] = Seq(),
-//                       subsequentCallingPoints: Seq[CallingPoint] = Seq(),
+                       private val previousCallingPointsWrapper: Seq[CallingPointWrapper] = Seq(),
+                       private val subsequentCallingPointsWrapper: Seq[CallingPointWrapper] = Seq(),
                        crs: String = "",
                        platform: String = "",
                        sta: String = "",
                        eta: String = "",
                        std: String = "",
                        etd: String = ""
-                     )
+                     ) {
+  val previousCallingPoints: Seq[CallingPoint] = previousCallingPointsWrapper.head.callingPoints
+  val subsequentCallingPoints: Seq[CallingPoint] = subsequentCallingPointsWrapper.head.callingPoints
+}
 
 object DetailedService {
   implicit val reads: Reads[DetailedService] = (
-//    (__ \ "previousCallingPoints"(0) \ "callingPoint").read[Seq[CallingPoint]] and
-//      (__ \ "subsequentCallingPoints"(0) \ "callingPoint").read[Seq[CallingPoint]] and
+    (__ \ "previousCallingPoints").read[Seq[CallingPointWrapper]] and
+      (__ \ "subsequentCallingPoints").read[Seq[CallingPointWrapper]] and
       (__ \ "crs").read[String] and
       (__ \ "platform").read[String] and
       (__ \ "sta").read[String] and
@@ -25,4 +28,13 @@ object DetailedService {
       (__ \ "std").read[String] and
       (__ \ "etd").read[String]
     )(DetailedService.apply _)
+}
+
+case class CallingPointWrapper(
+                              callingPoints: Seq[CallingPoint] = Seq()
+                              )
+
+object CallingPointWrapper {
+  implicit val reads: Reads[CallingPointWrapper] =
+    (__ \ "callingPoint").read[Seq[CallingPoint]] map CallingPointWrapper.apply
 }
