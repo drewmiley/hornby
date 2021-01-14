0b55088d551cf7be9b92a8b103465397bd1d7b80
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2021-01-14 00:23:50 +0000

    Implements DetailedService

--- app/hornby/models/DetailedService.scala
+++ app/hornby/models/DetailedService.scala
@@ -4,14 +4,24 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 case class DetailedService(
-                       dummy: String = "",
-                       dummy1: String = ""
+                       previousCallingPoints: Seq[CallingPoint] = Seq(),
+                       subsequentCallingPoints: Seq[CallingPoint] = Seq(),
+                       crs: String = "",
+                       sta: String = "",
+                       eta: String = "",
+                       std: String = "",
+                       etd: String = ""
                      )
 
 object DetailedService {
   implicit val reads: Reads[DetailedService] = (
-    (__ \ "dummy").read[String] and
-      (__ \ "dummy1").read[String]
+    (__ \ "previousCallingPoints"(0) \ "callingPoint").read[Seq[CallingPoint]] and
+      (__ \ "subsequentCallingPoints"(0) \ "callingPoint").read[Seq[CallingPoint]] and
+      (__ \ "crs").read[String] and
+      (__ \ "sta").read[String] and
+      (__ \ "eta").read[String] and
+      (__ \ "std").read[String] and
+      (__ \ "etd").read[String]
     )(DetailedService.apply _)
 }
 
