502215f1081f2f3c21aba3e9dfa312e183457d6f
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2021-01-14 00:34:36 +0000

    Adds hasCalledAt to CallingPoint model

--- app/hornby/models/CallingPoint.scala
+++ app/hornby/models/CallingPoint.scala
@@ -3,13 +3,14 @@ package hornby.models
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-// If et defined, hasCalledAt is true. Else hasCalledAt is false
 case class CallingPoint(
                        crs: String = "",
                        st: String = "",
                        et: Option[String] = None,
                        at: Option[String] = None
-                     )
+                     ) {
+  val hasCalledAt: Boolean = et.isDefined
+}
 
 object CallingPoint {
   implicit val reads: Reads[CallingPoint] = (
--- app/hornby/models/DetailedService.scala
+++ app/hornby/models/DetailedService.scala
@@ -24,23 +24,3 @@ object DetailedService {
       (__ \ "etd").read[String]
     )(DetailedService.apply _)
 }
-
-//case class Leaderboard(code: String = "", rows: Seq[LeaderboardRow] = Seq()) {
-//  val rowsWithPosition: Seq[LeaderboardRow] = rows
-//    .sorted(Ordering.by[LeaderboardRow, Int](_.score).reverse)
-//    .foldLeft(Seq[LeaderboardRow]())((acc, row) => {
-//      val position: Int = if (acc.nonEmpty && row.score == acc.last.score) {
-//        acc.last.positionAsString.toInt
-//      } else {
-//        acc.length + 1
-//      }
-//      acc :+ LeaderboardRow(row.score, row.user, Some(position))
-//    })
-//}
-//
-//object Leaderboard {
-//  implicit val reads: Reads[Leaderboard] = (
-//    (__ \ "code").read[String] and
-//      (__ \ "results").read[Seq[LeaderboardRow]]
-//    )(Leaderboard.apply _)
-//}
