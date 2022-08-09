90b856ac58242617089f1ce0446d66a1c50c94e5
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2022-08-09 23:53:51 +0100

    Modifications to run.py

--- run.py
+++ run.py
@@ -2,4 +2,6 @@ import requests
 
 getRequest = requests.get('http://localhost:9000/platforms/Newcastle')
 response = getRequest.json()
-print(response)
+print(response['stationName'])
+for service in response['services']:
+    print(service)
