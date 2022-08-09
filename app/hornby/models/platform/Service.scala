82f6becb5c112b9bb16cc196cf74488354a02de7
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2022-08-10 00:43:51 +0100

    Converts service to use case matches and no ifs

--- app/hornby/models/platform/Service.scala
+++ app/hornby/models/platform/Service.scala
@@ -15,33 +15,25 @@ object Service {
 
   // TODO: Can we make this implicit?
   def convert(huxleyService: DetailedService): Service = {
-    val pcp = huxleyService.previousCallingPoints.getOrElse(Seq())
-    val scp = huxleyService.subsequentCallingPoints.getOrElse(Seq())
-    val isOrigin = pcp.isEmpty
-    val isDestination = scp.isEmpty
-//    TODO: Can I avoid this if/else?
-//    Maybe with a case match
-    val callingAt = if (isOrigin) {
-      val originCallingPoint = CallingPoint(
-        huxleyService.crs,
-        huxleyService.scheduledDepartureTime.getOrElse(""),
-        actualTime = huxleyService.expectedDepartureTime
-      )
-      Seq(originCallingPoint) ++ pcp ++ scp
-    } else if (isDestination) {
-      val destinationCallingPoint = CallingPoint(
-        huxleyService.crs,
-        huxleyService.scheduledArrivalTime.getOrElse(""),
-        expectedTime = huxleyService.expectedArrivalTime
-      )
-      pcp ++ scp ++ Seq(destinationCallingPoint)
-    } else {
-      pcp ++ scp
+    val isOrigin = huxleyService.previousCallingPoints match {
+      case Some(a) if a.nonEmpty => false
+      case _ => true
     }
+    val isDestination = huxleyService.subsequentCallingPoints match {
+      case Some(a) if a.nonEmpty => false
+      case _ => true
+    }
+    val originCallingPoint = Option(isOrigin) collect { case true =>
+      CallingPoint(huxleyService.crs, huxleyService.scheduledDepartureTime.getOrElse(""), actualTime = huxleyService.expectedDepartureTime)}
+    val destinationCallingPoint = Option(isDestination) collect { case true =>
+      CallingPoint(huxleyService.crs, huxleyService.scheduledArrivalTime.getOrElse(""), expectedTime = huxleyService.expectedArrivalTime)
+    }
+//    TODO: Do this in a way not involving getOrElse
+    val callingAt = originCallingPoint.map(Seq(_)).getOrElse(Seq()) ++ huxleyService.previousCallingPoints.getOrElse(Seq()) ++ huxleyService.subsequentCallingPoints.getOrElse(Seq()) ++ destinationCallingPoint.map(Seq(_)).getOrElse(Seq())
     Service(
       huxleyService.platform,
-      if (isOrigin) { huxleyService.crs } else { pcp.head.crs },
-      if (isDestination) { huxleyService.crs } else { scp.last.crs },
+      if (isOrigin) { huxleyService.crs } else { huxleyService.previousCallingPoints.getOrElse(Seq()).head.crs },
+      if (isDestination) { huxleyService.crs } else { huxleyService.subsequentCallingPoints.getOrElse(Seq()).last.crs },
       callingAt.map(PlatformCallingPoint.convert)
     )
   }
